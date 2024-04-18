9055375347201b76dedd8ee33e3c38f6f4b6893c
Xuan Phuc Tran <124777191+XuanPhuc269@users.noreply.github.com>
2024-04-18 11:01:31 +0700

    Add files via upload

---
+++ CounterView.kt
@@ -0,0 +1,50 @@
+package com.example.counterapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TheCounterApp(viewModel: CounterViewModel) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Count: ${viewModel.count.value}",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+            Button(onClick = {
+                viewModel.increment()
+            }) {
+                Text(text = "Increment")
+            }
+
+            Button(onClick = {
+                viewModel.decrement()
+            }) {
+                Text(text = "Decrement")
+            }
+        }
+    }
+}
